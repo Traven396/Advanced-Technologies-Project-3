431127af8eb27a246272baef0ceb2dbc11a99b8b
Yeet Bigley <traven396@gmail.com>
2022-10-25 10:15:53 -0500

    I love john

--- src/main/java/com/aidyn_anton/john/John.java
+++ src/main/java/com/aidyn_anton/john/John.java
@@ -1,10 +1,24 @@
 package com.aidyn_anton.john;
 
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod("john")
 public class John {
+	
+	public static final String MOD_ID = "john";
+	
+	public static final CreativeModeTab John_Create_Tab = new CreativeModeTab("JohnTab") {
+		
+		@Override
+		public ItemStack makeIcon() {
+			// TODO Auto-generated method stub
+			return null;
+		}
+	};
+	
 
 	public John() {
 		MinecraftForge.EVENT_BUS.register(this);
